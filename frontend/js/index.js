2f4eb113370024b6dba4ac2915ddf5cbcfa8fb69
Dipak Khade <dipakhade214@gmail.com>
2025-03-15 19:07:41 +0530

    user profile page add

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,3 +9,7 @@ document.getElementById('nav2').addEventListener('click',()=>{
 document.getElementById('nav3').addEventListener('click',()=>{
      window.location.href = 'http://127.0.0.1:5500/frontend/index.html'
 })
+document.getElementById('nav4').addEventListener('click',()=>{
+     window.location.href = 'http://127.0.0.1:5500/frontend/user-profile.html'
+})
+
