2c360e8bab5e267a2002d949586aee2c052d4b7d
Dipak Khade <dipakhade214@gmail.com>
2025-03-22 00:02:14 +0530

    added cancled and add ticket

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,10 +6,10 @@ document.getElementById('nav1').addEventListener('click',()=>{
 document.getElementById('nav2').addEventListener('click',()=>{
      window.location.href = 'http://127.0.0.1:5500/frontend/book-ground.html'
 })
-document.getElementById('nav3').addEventListener('click',()=>{
-     window.location.href = 'http://127.0.0.1:5500/frontend/index.html'
-})
 document.getElementById('nav4').addEventListener('click',()=>{
      window.location.href = 'http://127.0.0.1:5500/frontend/user-profile.html'
+})
+document.getElementById('nav3').addEventListener('click',()=>{
+     window.location.href = 'http://127.0.0.1:5500/frontend/cancle-booking.html'
 })
 
